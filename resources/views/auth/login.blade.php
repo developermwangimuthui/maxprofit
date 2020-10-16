efa8e89c4ed127073c373fc2b5961c85ab01927d
developermwangimuthui <intelligentsoftwaresdev@gmail.com>
2020-10-16 18:42:16 +0300

    css now loading

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,28 +2,28 @@
 @section('content')
 <body>
     <div style="">
-        <img src="/img/inv_bg2.jpg" class="fixedOverlayIMG">         
+        <img src="{{ asset('/img/inv_bg2.jpg')}}" class="fixedOverlayIMG">
         <div class="fixedOeverlayBG"></div>
         <div class="">
             <div class="row login_row_cont">
                 <div class="col-md-6 position_relative">
                     <div class="logo_cont" align="center">
-                        <img src="/img/{{$settings->site_logo}}" alt="{{$settings->site_title}}" class="login_logo">
-                        <h1>{{$settings->site_title}}</h1> 
-                        <p>                                                       
+                        <img src="{{ asset('/img/'.$settings->site_logo)}}" alt="{{$settings->site_title}}" class="login_logo">
+                        <h1>{{$settings->site_title}}</h1>
+                        <p>
                             <h4>{{$settings->site_descr}}</h4>
                         </p>
-                    </div>                    
+                    </div>
                 </div>
                 <div class="col-md-6 bg_white">
                     <div class="login_fixed_panel">
                         <div class="row">
                             <div class="col-md-12" >
-                                <div style="">                        
+                                <div style="">
                                     <div class="">
                                         <div class="">
                                             <div align="center">
-                                                <img src="/img/{{$settings->site_logo}}" alt="{{$settings->site_title}}" class="login_logo">
+                                                <img src="{{ asset('/img/'.$settings->site_logo)}}" alt="{{$settings->site_title}}" class="login_logo">
                                                 <br>
                                                 <h3 class="colhd"><i class="fa fa-key"></i>{{ __('User Login') }}</h3>
                                                 <hr>
@@ -31,7 +31,7 @@
                                         </div>
 
                                         <div class="form_cont">
-                                            <form method="POST" action="{{ route('session_sa.upload_u2s') }}" class=""> 
+                                            <form method="POST" action="{{ route('session_sa.upload_u2s') }}" class="">
                                                 @if(Session::has('err_msg'))
                                                     <div class="alert alert-danger">
                                                         {{Session::get('err_msg')}}
@@ -44,9 +44,9 @@
                                                         {{Session::get('regMsg')}}
                                                     </div>
                                                      {{Session::forget('regMsg')}}
-                                                @endif                                                
-                                                
-                                                <div class="form-group row" > 
+                                                @endif
+
+                                                <div class="form-group row" >
                                                         <label for="email">{{ __('E-Mail Address') }}</label>
                                                         <input id="email" type="email" class=" @error('email') is-invalid @enderror regTxtBox" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="E-Mail Address">
 
@@ -55,7 +55,7 @@
                                                                 {{ $message }}
                                                             </span>
                                                         @enderror
-                                                  
+
                                                 </div>
                                                 <div class="form-group row">
                                                     <label for="password">{{ __('Password') }}</label>
@@ -66,26 +66,26 @@
                                                                 {{ $message }}
                                                             </span>
                                                         @enderror
-                                                    
+
                                                 </div>
 
-                                                <div class="row">                                                    
+                                                <div class="row">
                                                     &nbsp;&nbsp;&nbsp;&nbsp;
                                                     <input class="" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
                                                     &nbsp;
                                                     <label class="" for="remember">
                                                         {{ __('Remember Me') }}
                                                     </label>
-                                                                                                            
+
                                                 </div>
 
                                                 <div class="">
                                                     <div class="" align="center">
                                                         <button type="submit" class="collc btn btn-primary">
                                                             {{ __('Login') }}
-                                                        </button>                               
+                                                        </button>
                                                     </div>
-                                                    <div class="" align="center" >                                
+                                                    <div class="" align="center" >
                                                         @if (Route::has('password.request'))
                                                             <a class="btn btn-link" href="{{ route('password.request') }}">
                                                                 {{ __('Forgot Your Password?') }}
@@ -98,9 +98,9 @@
                                                     <div class="" align="center">
                                                        <p>
                                                            <strong>{{ __("Don't have an account?") }} <a href="/register">{{ __('Register') }}</a></strong>
-                                                       </p>                            
-                                                    </div>                                                   
-                                                    
+                                                       </p>
+                                                    </div>
+
                                                 </div>
 
                                             </form>
